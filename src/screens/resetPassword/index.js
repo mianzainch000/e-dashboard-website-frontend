7389b6378ee7f8aad0c08d44ff6c5adf00162c2d
mianzainch000 <mianzainch000@gmail.com>
2024-12-15 14:58:02 +0500

    reset password animation add

--- src/screens/resetPassword/index.js
+++ src/screens/resetPassword/index.js
@@ -12,6 +12,7 @@ import CustomButton from "../../components/CustomButton";
 import { NavLink, useSearchParams } from "react-router-dom";
 import LockRoundedIcon from "@mui/icons-material/LockRounded";
 import { useValidationSchemas } from "../../components/Validation";
+import PasswordUpdateSuccess from "../../components/PasswordUpdateSuccess";
 import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
 import VisibilityOffOutlinedIcon from "@mui/icons-material/VisibilityOffOutlined";
 import {
@@ -33,6 +34,7 @@ const ResetPassword = () => {
   const [loading, setLoading] = useState(false);
   const { resetFormValidation } = useValidationSchemas(t);
   const [showPassword, setShowPassword] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
@@ -73,10 +75,15 @@ const ResetPassword = () => {
 
       const res = await resetPassword(data, token);
       if (res?.status === 201) {
-        snackBarMessage({
-          type: "success",
-          message: res?.data?.message,
-        });
+        setSuccessMessage(res?.data.message);
+        setTimeout(() => {
+          setSuccessMessage("");
+        }, 3000);
+
+        // snackBarMessage({
+        //   type: "success",
+        //   message: res?.data?.message,
+        // });
         formik.handleReset();
       } else {
         snackBarMessage({
@@ -125,140 +132,144 @@ const ResetPassword = () => {
           }}
           className={styles.centeredContainer}
         >
-          <Box
-            component="form"
-            sx={{
-              width: "100%",
-              overflowY: "auto",
-            }}
-            className={styles.centeredContainer}
-            onSubmit={formik.handleSubmit}
-          >
-            {isMobile && (
-              <img
-                alt="logo"
-                src={logo}
-                style={{
-                  width: "30%",
-                  height: "100px",
-                  marginTop: "20px",
-                }}
-              />
-            )}
-
-            <Grid
-              container
-              spacing={2}
-              sx={{ marginTop: "10px", overflowY: "auto" }}
-              xs={12}
+          {successMessage ? (
+            <PasswordUpdateSuccess />
+          ) : (
+            <Box
+              component="form"
+              sx={{
+                width: "100%",
+                overflowY: "auto",
+              }}
+              className={styles.centeredContainer}
+              onSubmit={formik.handleSubmit}
             >
-              <Grid item xs={12} className={styles.centeredContainer}>
-                <Typography
-                  color={"--text-dark"}
-                  fontSize={"20px"}
-                  fontWeight={"bolder"}
-                >
-                  {t("RESET_PASSWORD")}
-                </Typography>
-              </Grid>
+              {isMobile && (
+                <img
+                  alt="logo"
+                  src={logo}
+                  style={{
+                    width: "30%",
+                    height: "100px",
+                    marginTop: "20px",
+                  }}
+                />
+              )}
 
-              <Grid item xs={12} className={styles.centeredContainer}>
-                <TextInput
-                  name="password"
-                  label={t("PASSWORD")}
-                  type={showPassword ? "text" : "password"}
-                  icon={
-                    <IconButton edge="start">
-                      <LockRoundedIcon sx={{ color: "var(--text-dark)" }} />
-                    </IconButton>
-                  }
-                  endIcon={
-                    <IconButton
-                      onClick={handleTogglePasswordVisibility}
-                      edge="end"
-                    >
-                      {showPassword ? (
-                        <VisibilityOutlinedIcon
-                          sx={{ color: "var(--text-dark)" }}
-                        />
-                      ) : (
-                        <VisibilityOffOutlinedIcon
-                          sx={{ color: "var(--text-dark)" }}
-                        />
-                      )}
-                    </IconButton>
-                  }
-                  value={formik.values.password}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  error={
-                    formik.touched.password && Boolean(formik.errors.password)
-                  }
-                />
-                {formik.touched.password && formik.errors.password ? (
-                  <Typography className={styles.error}>
-                    {formik.errors.password}
+              <Grid
+                container
+                spacing={2}
+                sx={{ marginTop: "10px", overflowY: "auto" }}
+                xs={12}
+              >
+                <Grid item xs={12} className={styles.centeredContainer}>
+                  <Typography
+                    color={"--text-dark"}
+                    fontSize={"20px"}
+                    fontWeight={"bolder"}
+                  >
+                    {t("RESET_PASSWORD")}
                   </Typography>
-                ) : null}
-              </Grid>
-              <Grid item xs={12} className={styles.centeredContainer}>
-                <TextInput
-                  name="confirmPassword"
-                  label={t("CONFIRM_PASSWORD")}
-                  type={showConfirmPassword ? "text" : "password"}
-                  icon={
-                    <IconButton edge="start">
-                      <LockRoundedIcon sx={{ color: "var(--text-dark)" }} />
-                    </IconButton>
-                  }
-                  endIcon={
-                    <IconButton
-                      onClick={handleToggleConfirmPasswordVisibility}
-                      edge="end"
-                    >
-                      {showConfirmPassword ? (
-                        <VisibilityOutlinedIcon
-                          sx={{ color: "var(--text-dark)" }}
-                        />
-                      ) : (
-                        <VisibilityOffOutlinedIcon
-                          sx={{ color: "var(--text-dark)" }}
-                        />
-                      )}
-                    </IconButton>
-                  }
-                  value={formik.values.confirmPassword}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  error={
-                    formik.touched.confirmPassword &&
-                    Boolean(formik.errors.confirmPassword)
-                  }
-                />
-                {formik.touched.confirmPassword &&
-                formik.errors.confirmPassword ? (
-                  <Typography className={styles.error}>
-                    {formik.errors.confirmPassword}
-                  </Typography>
-                ) : null}
-              </Grid>
+                </Grid>
 
-              <Grid item xs={12} className={styles.centeredContainer}>
-                <CustomButton
-                  title={t("RESET_PASSWORD")}
-                  loading={loading}
-                  type="submit"
-                />
+                <Grid item xs={12} className={styles.centeredContainer}>
+                  <TextInput
+                    name="password"
+                    label={t("PASSWORD")}
+                    type={showPassword ? "text" : "password"}
+                    icon={
+                      <IconButton edge="start">
+                        <LockRoundedIcon sx={{ color: "var(--text-dark)" }} />
+                      </IconButton>
+                    }
+                    endIcon={
+                      <IconButton
+                        onClick={handleTogglePasswordVisibility}
+                        edge="end"
+                      >
+                        {showPassword ? (
+                          <VisibilityOutlinedIcon
+                            sx={{ color: "var(--text-dark)" }}
+                          />
+                        ) : (
+                          <VisibilityOffOutlinedIcon
+                            sx={{ color: "var(--text-dark)" }}
+                          />
+                        )}
+                      </IconButton>
+                    }
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    error={
+                      formik.touched.password && Boolean(formik.errors.password)
+                    }
+                  />
+                  {formik.touched.password && formik.errors.password ? (
+                    <Typography className={styles.error}>
+                      {formik.errors.password}
+                    </Typography>
+                  ) : null}
+                </Grid>
+                <Grid item xs={12} className={styles.centeredContainer}>
+                  <TextInput
+                    name="confirmPassword"
+                    label={t("CONFIRM_PASSWORD")}
+                    type={showConfirmPassword ? "text" : "password"}
+                    icon={
+                      <IconButton edge="start">
+                        <LockRoundedIcon sx={{ color: "var(--text-dark)" }} />
+                      </IconButton>
+                    }
+                    endIcon={
+                      <IconButton
+                        onClick={handleToggleConfirmPasswordVisibility}
+                        edge="end"
+                      >
+                        {showConfirmPassword ? (
+                          <VisibilityOutlinedIcon
+                            sx={{ color: "var(--text-dark)" }}
+                          />
+                        ) : (
+                          <VisibilityOffOutlinedIcon
+                            sx={{ color: "var(--text-dark)" }}
+                          />
+                        )}
+                      </IconButton>
+                    }
+                    value={formik.values.confirmPassword}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    error={
+                      formik.touched.confirmPassword &&
+                      Boolean(formik.errors.confirmPassword)
+                    }
+                  />
+                  {formik.touched.confirmPassword &&
+                  formik.errors.confirmPassword ? (
+                    <Typography className={styles.error}>
+                      {formik.errors.confirmPassword}
+                    </Typography>
+                  ) : null}
+                </Grid>
+
+                <Grid item xs={12} className={styles.centeredContainer}>
+                  <CustomButton
+                    title={t("RESET_PASSWORD")}
+                    loading={loading}
+                    type="submit"
+                  />
+                </Grid>
+                <Grid item>
+                  <NavLink to="/" variant="body2">
+                    <Typography className={styles.goBackToLogin}>
+                      {t("GO_BACK_TO_LOGIN")}
+                    </Typography>
+                  </NavLink>
+                </Grid>
               </Grid>
-              <Grid item>
-                <NavLink to="/" variant="body2">
-                  <Typography className={styles.goBackToLogin}>
-                    {t("GO_BACK_TO_LOGIN")}
-                  </Typography>
-                </NavLink>
-              </Grid>
-            </Grid>
-          </Box>
+            </Box>
+          )}
         </Box>
       </Grid>
     </Grid>
